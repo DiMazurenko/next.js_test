209ddff4517f1002fa30daf195fd6762aa5a7091
Dmitriy <dimazurenko@gmail.com>
2023-08-18 17:45:29 +0300

    init project

---
+++ admin-panel/app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import {Button} from "@/components/ui/button";
+
+export default function Home() {
+    return (
+        <main>
+            <h1>Hello</h1>
+            <Button>Click</Button>
+        </main>
+    )
+}
